4dd1384323dc9c334a65092e056d71413a1d60c2
Bartłomiej Rzepka <bartlomiejrzepka@wp.pl>
2022-01-05 13:19:20 +0100

    chore: some improvements to line and area charts

--- src/plot/AreaPlot.ts
+++ src/plot/AreaPlot.ts
@@ -1,4 +1,4 @@
-import { SerieOptionsLine, SerieOptionsShape } from "../model/types";
+import { SerieOptionsLine, SerieOptionsPoint, SerieOptionsShape } from "../model/types";
 import LinePlot from "./LinePlot";
 import PlotKit from "./plotKits/PlotKit";
 import { DataForSerieDrawing } from "./types";
@@ -7,30 +7,43 @@ export default class AreaPlot extends LinePlot {
     PIXEL_PADDING = 0;
 
     protected override performDrawing(data: DataForSerieDrawing): void {
-        const { series, mappers, labelFrame } = data;
-        const bottom = mappers.yFunc(data.yMinForSeries);
+        const { series, mappers, labelFrame, yMinForSeries } = data;
+        const bottom = mappers.yFunc(yMinForSeries);
+
         if (this.plotKit == undefined)
             this.plotKit = new PlotKit(this.ctx, this);
-        series.forEach(serie => {
-            this.plotKit.lineTools.setLineStyle(serie.options as SerieOptionsLine);
-            this.plotKit.patternTools.applyShapeOrColor(this.ctx, (serie.options as SerieOptionsShape).shape, serie.options.color);
+
+        for (let serieIndex = 0; serieIndex < series.length; serieIndex++) {
+            const pointsInSerie = series[serieIndex].points.length;
+            if (pointsInSerie < 2) continue;
             const path = new Path2D();
-            serie.points.forEach((point, index) => {
-                const mappedPoint = this.mapSpacePointToPixelPoint(point, mappers.xFunc, mappers.yFunc);
-                if (index == 0) {
-                    path.moveTo(mappedPoint.x, bottom);
-                    path.lineTo(mappedPoint.x, mappedPoint.y);
-                } else if (index == serie.points.length - 1) {
-                    path.lineTo(mappedPoint.x, mappedPoint.y);
-                    path.lineTo(mappedPoint.x, bottom);
-                    path.closePath();
-                } else {
-                    path.lineTo(mappedPoint.x, mappedPoint.y);
-                }
-                this.drawValueForPoint(point.x, mappedPoint.x, labelFrame);
-            });
+            const { points, options } = series[serieIndex];
+
+            const firstPoint = this.mapSpacePointToPixelPoint(points[0], mappers.xFunc, mappers.yFunc);
+            path.moveTo(firstPoint.x, bottom);
+            path.lineTo(firstPoint.x, firstPoint.y);
+            this.drawValueForPoint(points[0].x, firstPoint.x, labelFrame);
+            this.plotKit.pointTools.performDrawPoint(firstPoint, options as SerieOptionsPoint);
+
+            for (let pointIndex = 1; pointIndex < pointsInSerie - 1; pointIndex++) {
+                const mappedPoint = this.mapSpacePointToPixelPoint(points[pointIndex], mappers.xFunc, mappers.yFunc);
+                path.lineTo(mappedPoint.x, mappedPoint.y);
+                this.drawValueForPoint(points[pointIndex].x, mappedPoint.x, labelFrame);
+                this.plotKit.pointTools.performDrawPoint(firstPoint, options as SerieOptionsPoint);
+            }
+
+            const lastPoint = this.mapSpacePointToPixelPoint(points[pointsInSerie - 1], mappers.xFunc, mappers.yFunc);
+            path.lineTo(lastPoint.x, lastPoint.y);
+            path.lineTo(lastPoint.x, bottom);
+            this.drawValueForPoint(points[pointsInSerie - 1].x, firstPoint.x, labelFrame);
+            this.plotKit.pointTools.performDrawPoint(lastPoint, options as SerieOptionsPoint);
+
+            this.plotKit.lineTools.setLineStyle(options as SerieOptionsLine);
+            this.plotKit.patternTools.applyShapeOrColor(this.ctx, (options as SerieOptionsShape).shape, options.color);
             this.ctx.fill(path);
-        });
+            this.ctx.strokeStyle = 'black';
+            this.ctx.stroke(path);
+        }
         this.ctx.setLineDash([]);
     }
 }
--- src/plot/LinePlot.ts
+++ src/plot/LinePlot.ts
@@ -1,4 +1,4 @@
-import { SerieOptionsLine } from "../model/types";
+import { SerieOptionsLine, SerieOptionsPoint } from "../model/types";
 import PlotKit from "./plotKits/PlotKit";
 import PointPlot from "./PointPlot";
 import { DataForSerieDrawing } from "./types";
@@ -6,24 +6,31 @@ import { DataForSerieDrawing } from "./types";
 export default class LinePlot extends PointPlot {
     protected override performDrawing(data: DataForSerieDrawing): void {
         const { series, mappers, labelFrame } = data;
+
         if (this.plotKit == undefined)
             this.plotKit = new PlotKit(this.ctx, this);
-        series.forEach(serie => {
-            this.plotKit.lineTools.setLineStyle(serie.options as SerieOptionsLine);
-            this.ctx.beginPath();
-            serie.points.forEach((point, index) => {
-                const mappedPoint = this.mapSpacePointToPixelPoint(point, mappers.xFunc, mappers.yFunc);
-                if (index == 0) {
-                    this.ctx.moveTo(mappedPoint.x, mappedPoint.y);
-                } else {
-                    this.ctx.lineTo(mappedPoint.x, mappedPoint.y);
-                    this.ctx.moveTo(mappedPoint.x, mappedPoint.y);
-                }
-                this.drawValueForPoint(point.x, mappedPoint.x, labelFrame);
-            });
-            this.ctx.closePath();
-            this.ctx.stroke();
-        });
+
+        for (let serieIndex = 0; serieIndex < series.length; serieIndex++) {
+            const pointsInSerie = series[serieIndex].points.length;
+            if (pointsInSerie < 2) continue;
+            const path = new Path2D();
+            const { points, options } = series[serieIndex];
+
+            const firstPoint = this.mapSpacePointToPixelPoint(points[0], mappers.xFunc, mappers.yFunc);
+            path.moveTo(firstPoint.x, firstPoint.y);
+            this.drawValueForPoint(points[0].x, firstPoint.x, labelFrame);
+            this.plotKit.pointTools.performDrawPoint(firstPoint, options as SerieOptionsPoint);
+
+            for (let pointIndex = 1; pointIndex < pointsInSerie; pointIndex++) {
+                const mappedPoint = this.mapSpacePointToPixelPoint(points[pointIndex], mappers.xFunc, mappers.yFunc);
+                path.lineTo(mappedPoint.x, mappedPoint.y);
+                this.drawValueForPoint(points[pointIndex].x, mappedPoint.x, labelFrame);
+                this.plotKit.pointTools.performDrawPoint(mappedPoint, options as SerieOptionsPoint);
+            }
+
+            this.plotKit.lineTools.setLineStyle(options as SerieOptionsLine);
+            this.ctx.stroke(path);
+        }
         this.ctx.setLineDash([]);
     }
 }
